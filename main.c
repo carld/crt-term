437104af725b9b3b370f50081e227e152ba39dfc
carl <carl.douglas@gmail.com>
2016-03-28 13:50:36 +1300

    check for key press

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <GLFW/glfw3.h>
 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
 int main(void)
 {
   GLFWwindow* window;
@@ -19,6 +25,9 @@ int main(void)
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
 
+  glfwSwapInterval(1);
+  glfwSetKeyCallback(window, key_callback);
+
   /* Loop until the user closes the window */
   while (!glfwWindowShouldClose(window))
   {
@@ -30,7 +39,7 @@ int main(void)
     /* Poll for and process events */
     glfwPollEvents();
   }
-
+  glfwDestroyWindow(window);
   glfwTerminate();
   return 0;
 }
