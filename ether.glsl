76b4034f873e71537bc3acc8ab1644545bacab5b
carl <carl.douglas@gmail.com>
2016-04-07 23:48:05 +1200

    another shader

---
+++ ether.glsl
@@ -0,0 +1,37 @@
+//Ether by nimitz (twitter: @stormoid)
+#version 150
+
+in vec3 Color;
+in vec2 Texcoord;
+out vec4 outColor;
+uniform sampler2D tex;
+uniform float appTime;
+uniform vec2 sourceSize;
+uniform vec2 targetSize;
+
+#define t appTime
+mat2 m(float a){float c=cos(a), s=sin(a);return mat2(c,-s,s,c);}
+float map(vec3 p){
+    p.xz*= m(t*0.4);p.xy*= m(t*0.3);
+    vec3 q = p*2.+t*1.;
+    return length(p+vec3(sin(t*0.7)))*log(length(p)+1.) + sin(q.x+sin(q.z+sin(q.y)))*0.5 - 1.;
+}
+
+void main(){  
+  vec2 p = gl_FragCoord.xy/targetSize.y - vec2(.9,.5);
+    vec3 cl = vec3(0.);
+    float d = 2.5;
+    for(int i=0; i<=5; i++) {
+    vec3 p = vec3(0,0,5.) + normalize(vec3(p, -1.))*d;
+        float rz = map(p);
+    float f =  clamp((rz - map(p+.1))*0.5, -.1, 1. );
+        vec3 l = vec3(0.1,0.3,.4) + vec3(5., 2.5, 3.)*f;
+        cl = cl*l + (1.-smoothstep(0., 2.5, rz))*.7*l;
+    d += min(rz, 1.);
+  }
+  vec4 dst = vec4(cl, 1.);
+  vec4 src = texture(tex, Texcoord);
+  src.a = ceil(src.rgb);
+  outColor = src + dst;
+}
+
